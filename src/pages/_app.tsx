bf0946dcf5d6cc68ca05d9bbb9da13302e198502
JonghwanWon <jh.wwwon@gmail.com>
2022-06-26 00:00:37 +0900

    feat:install `tailwindcss`

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,7 @@
-import axios from 'axios';
-import App, { AppContext, AppProps } from 'next/app';
+import '~/styles/globals.css';
+
+import { AppProps } from 'next/app';
 import Head from 'next/head';
-
-import { initAxiosConfig } from '~/utils/requestClient';
-
-initAxiosConfig();
 
 const MyApp = ({ Component, pageProps }: AppProps) => {
   return (
@@ -12,7 +9,6 @@ const MyApp = ({ Component, pageProps }: AppProps) => {
       <Head>
         <meta charSet="utf-8" key="charSet" />
         <meta httpEquiv="X-UA-Compatible" content="IE=edge" key="httpEquiv" />
-        <meta name="format-detection" content="telephone=no" />
         <meta
           name="viewport"
           content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0"
@@ -21,21 +17,6 @@ const MyApp = ({ Component, pageProps }: AppProps) => {
       <Component {...pageProps} />
     </>
   );
-};
-
-MyApp.getInitialProps = async (appContext: AppContext) => {
-  const { ctx } = appContext;
-  const cookie = ctx.req ? ctx.req.headers.cookie : null;
-
-  // INFO: token
-  axios.defaults.headers.common.Cookie = '';
-  if (cookie) {
-    axios.defaults.headers.common.Cookie = cookie;
-  }
-
-  const appProps = await App.getInitialProps(appContext);
-
-  return { ...appProps };
 };
 
 export default MyApp;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,9 @@
 const Page = () => {
-  return <div>Hello World</div>;
+  return (
+    <div>
+      <h1 className="text-4xl text-blue-700">Hello World</h1>
+    </div>
+  );
 };
 
 export default Page;
